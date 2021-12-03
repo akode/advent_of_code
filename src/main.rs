dcb81c7cf348fd5e9593edc6205c5ae3deaf86ff
akode <andreas.kodewitz@gmail.com>
2021-12-03 17:53:28 +0100

    day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 use std::fs::File;
 use std::io::{BufReader, Error, ErrorKind};
 use std::io::prelude::*;
+use std::ops;
 
+#[derive(Debug)]
+struct Pos {
+    h: i32,
+    d: i32
+}
+
+impl ops::Add<Pos> for Pos {
+    type Output = Pos;
+    fn add(self, rhs: Pos) -> Pos {
+        Pos {
+            h: self.h + rhs.h,
+            d: self.d + rhs.d
+        }
+    }
+}
+
+fn parse_line(line: std::io::Result<String>) -> Pos {
+    let l = line.unwrap();
+    let mut iter = l.split_whitespace();
+    let direction = iter.next().unwrap().to_string();
+    let amount = iter.next().unwrap().parse().unwrap();
+    match direction.as_ref() {
+        "forward" => Pos {h: amount, d: 0},
+        "up" => Pos {h: 0, d: -amount},
+        "down" => Pos {h: 0, d: amount},
+        _ => panic!("Unknown direction")
+    }
+}
 
 fn day1p1() -> Result<u32, Error> {
     let f = File::open("./data/input1.txt")?;
@@ -25,7 +54,16 @@ fn day1p2() -> Result<u32, Error> {
     Ok(smoothe_depths)
 }
 
+fn day2p1() -> Result<i32, Error> {
+    let f = File::open("./data/input2.txt");
+    let br = BufReader::new(f.unwrap());
+
+    let final_pos = br.lines().map(|item| parse_line(item)).fold(Pos{h:0,d:0}, |acc, x| acc + x);
+    Ok(final_pos.h * final_pos.d)
+}
+
 fn main() -> () {
     println!("Day 1 part 1 result: {}", day1p1().unwrap());
     println!("Day 1 part 2 result: {}", day1p2().unwrap());
+    println!("Day 2 part 1 result: {}", day2p1().unwrap());
 }
