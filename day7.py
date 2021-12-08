31b84959da9f5494dca185046c65a34f9b44bbb9
Andreas Kodewitz <andreas.kodewitz@burda-forward.de>
2021-12-08 09:46:44 +0100

    day 7 python solution

--- day7.py
+++ day7.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 
 def load_data():
     with open("./data/input7.txt") as fp:
@@ -9,9 +10,27 @@ def find_min_pos(data):
     dist = [sum(abs(v-i) for v in data) for i in range(max_val)]
     return min(dist)
 
+@lru_cache
+def dist2cost(dist):
+    return sum(range(1,dist+1))
+
+def find_min_pos_p2(data):
+    max_val = max(data)
+    cost = [0]*max_val
+    for i in range(max_val):
+        for v in data:
+            dist = abs(v-i)
+            cost[i] += dist2cost(dist)
+    return min(cost)
+
 def part1():
     data = load_data()
     return find_min_pos(data)
 
+def part2():
+    data = load_data()
+    return find_min_pos_p2(data)
+
 if __name__ == "__main__":
     print(f"Day 7 part 1 result: {part1()}")
+    print(f"Day 7 part 2 result: {part2()}")
