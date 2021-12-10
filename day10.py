65f65ed534c2688f74cd4611e78ac983b02aa0ce
Andreas Kodewitz <andreas.kodewitz@burda-forward.de>
2021-12-10 23:12:46 +0100

    day 10 solution complete

--- day10.py
+++ day10.py
@@ -18,13 +18,27 @@ pairs = {
     "]": "[",
     ">": "<"
 }
+
+close = {value: key for key, value in pairs.items()}
+
+def completion_score(completion):
+    value = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4
+    }
+    total = 0
+    for item in completion:
+        total = total*5 + value[item]
+    return total
     
 def part1():
     data = load_data()
-    stack = deque()
     score = 0
-    corrupted = 0
+    non_corrupted = []
     for line in data:
+        stack = deque()
         line = line.strip()
         for item in line:
             if item in "{[<(":
@@ -32,17 +46,28 @@ def part1():
             else:
                 if stack.pop() != pairs[item]:
                     score += cost[item]
-                    corrupted += 1
+                    stack.clear()
                     break
+        if len(stack) != 0:
+            non_corrupted.append(line)
 
-        
-    print(corrupted)
-    return score
+    return score, non_corrupted
 
-def part2():
-    data = load_data()
-    return None
+def part2(non_corrupted):
+    completion_scores = []
+    for line in non_corrupted:
+        stack = deque()
+        for item in line:
+            if item in "{[<(":
+                stack.append(item)
+            else:
+                stack.pop()
+        completion = [close[item] for item in reversed(stack)]
+        completion_scores.append(completion_score(completion))
+    completion_scores = sorted(completion_scores)
+    return completion_scores[len(completion_scores)//2]
 
 if __name__ == "__main__":
-    print(f"Result day 10 part 1: {part1()}")
-    print(f"Result day 10 part 2: {part2()}")
+    score, non_corrupted = part1()
+    print(f"Result day 10 part 1: {score}")
+    print(f"Result day 10 part 2: {part2(non_corrupted)}")
